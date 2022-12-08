d56b00e3bba44313eba5dbabacc7d3dc21a5b45a
Isaiah Gordon <GordIsai857@GoTVDSB.ca>
2022-12-08 09:34:51 -0500

    added reading me

--- Game1.cs
+++ Game1.cs
@@ -13,17 +13,19 @@ namespace Finaly_of_mine
         MouseState mouseState;
         Levels level;
         SpriteFont font;
-        Texture2D myTexture;
-        Rectangle myRect;
+        Texture2D grassText,playText;
+        Rectangle myRect,playRect,plusRect;
+        Vector2 vect,gravite,grassPlus;
         enum Screen
         {
             Intro,
             Middle,
+            Game,
             Endtro
         }
         enum Levels
         {
-            One,Two,Three,Four
+            Zero,One,Two,Three,Four
         }
         public Game1()
         {
@@ -36,30 +38,37 @@ namespace Finaly_of_mine
         {
             // TODO: Add your initialization logic here
             screen=Screen.Intro;
-            myRect=new Rectangle(0,0,75,75);
-            base.Initialize();
-            
+            level=Levels.Zero;
+            vect = new Vector2(100, 200);
+            myRect =new Rectangle(0,375,75,105);
+            playRect = new Rectangle(0, 300, 75, 75);            
+            gravite = new Vector2(0, -100);
+            grassPlus = new Vector2(75, 0);
+            plusRect = new Rectangle((int)(myRect.X + grassPlus.X), myRect.Y, myRect.Width, myRect.Height);
+            base.Initialize();            
         }
 
         protected override void LoadContent()
         {
             spriBat = new SpriteBatch(GraphicsDevice);
-            myTexture = Content.Load<Texture2D>("1stTexture");
+            grassText = Content.Load<Texture2D>("Grass");
             font = Content.Load<SpriteFont>("File");
+            playText = Content.Load<Texture2D>("download");
             // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
         {
             mouseState = Mouse.GetState();
+            KeyboardState kstate = Keyboard.GetState();
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
             if (screen == Screen.Intro)
                 if (mouseState.LeftButton == ButtonState.Pressed)
                     screen = Screen.Middle;
             if (screen == Screen.Middle)
-                if (mouseState.LeftButton == ButtonState.Pressed)
-                    level =Levels.One;            
+                if (kstate.IsKeyDown(Keys.A))
+                { level = Levels.One; screen = Screen.Game; }         
             if(level==Levels.Four)
               if(mouseState.LeftButton == ButtonState.Pressed)
                     screen = Screen.Endtro;           
@@ -79,16 +88,22 @@ namespace Finaly_of_mine
             // TODO: Add your drawing code here
             spriBat.Begin();
             if(screen == Screen.Intro)
-            {
-                //spriBat.Draw();
+            {                
+                spriBat.DrawString(font,"Left clicks to continue",vect,Color.Blue);
             }
             if(screen == Screen.Middle)
             {
-                //spriBat.Draw();
+                spriBat.DrawString(font, "Wasd to play, A to continue", vect, Color.Blue);
             }
             if(level==Levels.One)
             {
-                //spriBat.Draw();
+                spriBat.Draw(grassText, myRect, Color.White);
+                for (int i = 0; i < 10; i++)
+                { 
+
+                    spriBat.Draw(grassText, plusRect, Color.White); 
+                }              
+                spriBat.Draw(playText, playRect, Color.White);
             }
             if (screen == Screen.Endtro)
             {
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Finaly of mine
+classes
+addm in tribbles
+and put them in Texture2D
+and copy and past the relavent code
+go to project add class c# call it trible
+click add notece tribble.cs
+copy namespace from program.cs
+revome internal
+private rectangle  callit rectangle
+private vector2 speed
+private texeure2d texture
+public trible(rect,vect2,text2d)
+{
+	rectangle=rect;
+	speed=vect2;
+	texture=text2d;
+}
+public texture2d texture
+{get{return texture;}}
+public rectanlge bounds{get{return rect;}set{rect=value;}}
+public void move(){rect.offset(speed);}
+no need of rect or speed just text, go to iniulixe methoud
+after base.it;
+declare tribbles
+new trible(put in the things u made add tribble texture)
+go to up date trible.move;
+draw trble text,trible rect,bribble color.white;
+go bach to move()
+what we had befor now here

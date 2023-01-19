4aeb9f81ee8fb11675226d02c5037eff1f2936eb
Isaiah Gordon <GordIsai857@GoTVDSB.ca>
2023-01-19 09:23:11 -0500

    Newest Rightt Here

--- Game1.cs
+++ Game1.cs
@@ -14,7 +14,7 @@ namespace Finaly_of_mine
         MouseState mouseState;        
         SpriteFont font;
         Player player;       
-        Texture2D boxText,lockText;        
+        Texture2D boxText,lockText,whiteText;        
         Vector2 gravite,vect;
         Box box;
         Lock locked;
@@ -50,7 +50,7 @@ namespace Finaly_of_mine
             base.Initialize();
             player = new Player( new Vector2(12.5f, 12.5f));           
             box = new Box(boxText, new Rectangle(graph.PreferredBackBufferWidth/2-50,graph.PreferredBackBufferHeight/2-50, 100, 100), Color.White);
-            locked = new Lock(lockText, Color.White, new Rectangle(0, 0, 25, 25));
+            locked = new Lock(lockText, Color.White, new Rectangle(0, 0, 25, 25),whiteText);
         }
 
         protected override void LoadContent()
@@ -58,7 +58,8 @@ namespace Finaly_of_mine
             spriBat = new SpriteBatch(GraphicsDevice);            
             font = Content.Load<SpriteFont>("File");
             boxText = Content.Load<Texture2D>("woodside");
-            lockText = Content.Load<Texture2D>("comlock");
+            lockText = Content.Load<Texture2D>("3dCl");
+            whiteText = Content.Load<Texture2D>("white");
             // TODO: use this.Content to load your game content here
         }
 
--- Lock.cs
+++ Lock.cs
@@ -11,11 +11,13 @@ namespace Finaly_of_mine
 {
     class Lock
     {
-        private Texture2D _texture;
+        
+        private Texture2D _texture,_texture2;
         private Color _colour;
         private bool _locked;
-        private Rectangle _bounds;
+        private Rectangle _bounds,_bounds2=new Rectangle(0,200,50,50);
         private string _locknum;
+        private Vector2 _vect,_vector=new Vector2(250,50);
         public void thelock(bool b)
         {
             if (b == true)
@@ -35,7 +37,9 @@ namespace Finaly_of_mine
             set{ _bounds = value; }
         }        
         public string Locknum
-        { get { return _locknum; } }
+        { 
+            get { return _locknum; }            
+        }
         public bool locked(Player.Room room,MouseState ms,Random r,KeyboardState ks)
         {
             int i, n, t;
@@ -44,29 +48,57 @@ namespace Finaly_of_mine
             if (room == Player.Room.Start && ms.LeftButton == ButtonState.Pressed)
             { 
                 Bounds=new Rectangle(Bounds.X,Bounds.Y,200,200);
-                i = r.Next(0, 60);
-                n= r.Next(0, 60);
-                t= r.Next(0, 60);
+                i = r.Next(10);
+                n= r.Next(10);
+                t= r.Next(10);
+                
+                
                 return true;
             }
             else
                 return false;
         }
+        private int[] _ints = new int[10];
         public Vector2 Vect(GraphicsDeviceManager graph)
         {
-            Vector2 v = new Vector2(graph.PreferredBackBufferWidth,graph.PreferredBackBufferHeight);
+            Vector2 v = new Vector2(graph.PreferredBackBufferWidth, graph.PreferredBackBufferHeight);
+            Vect(v);
             return v;
         }
-        private Vector2 Vect;
-        
-        
-        
+        private Vector2 Vect(Vector2 vector)
+        {
+            _vect=vector;
+            return vector;
+        }
+        public Vector2 DrawingVector
+        {
+            get { return _vect; }
+        }       
         public void Draw(SpriteBatch sb,SpriteFont font)
         {
-            sb.Draw(Texture, Bounds, Colour);
-            sb.DrawString(font, Locknum,);
+            sb.Draw(Texture,Bounds,Colour);
+            sb.DrawString(font,Locknum,DrawingVector,_colour);
+            foreach (int i in _ints)
+            {
+                _vector = new Vector2(_bounds2.Y, _bounds2.Width);
+                sb.Draw(_texture,_bounds2,Color.LightGray);
+                sb.DrawString(font,i.ToString(),_vector,Color.Black);
+                _bounds2.X += 50;
+                _vector += new Vector2(50, 50);
+                if (i == 3 || i == 6)
+                {
+                    _bounds2.X = 0;
+                    _bounds2.Y += 50;
+                    _vector = new Vector2(_bounds2.Y, _bounds2.Width);
+                }
+                else if (i == 9)
+                {
+                    _bounds2.X = 50;
+                    _vector = new Vector2(_bounds2.Y, _bounds2.Width);
+                }                
+            }
         }
-        public Lock(Texture2D texture, Color colour, Rectangle bounds)
+        public Lock(Texture2D texture, Color colour, Rectangle bounds,Texture2D texture2)
         {
             _texture = texture;
             _colour = colour;            
